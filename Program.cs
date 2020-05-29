443761c5fc5502b9be1b4cde93b039251d4aae50
ViniciusMirandaDev <ViniciusMirandaDev>
2020-05-29 15:24:41 -0300

    Adicionais feitos

--- Program.cs
+++ Program.cs
@@ -13,9 +13,17 @@ namespace CpfCpnj_poo
             cpf.cpf = "000.000.000-00";
             System.Console.WriteLine(cpf.ValidarCPF());
 
+            
             // Adicionais
-
+            System.Console.WriteLine("Digite seu nome:");
             cpf.nome= Console.ReadLine();
+            Console.Clear();
+            
+            System.Console.WriteLine(cpf.Saudar());
+
+            System.Console.WriteLine("Para vermos se seu cpf é verdadeiro utilizamos o método True or False, portanto digite seu cpf para confirmarmos:");
+            cpf.cpf=Console.ReadLine();
+            System.Console.WriteLine(cpf.ValidarCPF());
             
         }
     }
